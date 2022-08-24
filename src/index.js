4eaa15358b7bae3d8479d197b276cea1a48c3069
francisco-solis99 <franciscosoliscss@gmail.com>
2022-08-24 16:31:53 -0500

    [Fix] how to delete the tags using an object

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 
 const tagsComponent = document.querySelector('.tags__component');
+// save tags
 const tagsList = [];
 renderComponent();
+
+
 function renderComponent(){
   tagsComponent.innerHTML = `
-    <h1 class="tags__title">Add your tags (at least 3 and maximum 5)</h1>
+    <h1 class="tags__title">Add your tags</h1>
     <div class="tags__zone">
       <div class="tags__wrapper"></div>
       <input type="text" class="tags__input">
@@ -16,44 +19,50 @@ function renderComponent(){
 function handlerKeyDown(e) {
   const key = e.key.toLowerCase();
   if(key !== 'enter' && key !== 'backspace') return;
-  const currentVal = e.target.value;
+  const currentVal = e.target.value.split(' ').join('-');
 
   // If enter key
-  if(key === 'enter') {
-    if(!currentVal) return;
+  if(key === 'enter' && currentVal && !isReapeated(currentVal)) {
     renderTag(currentVal);
     e.target.value = '';
     return;
   }
   // if backspace key
-  if(currentVal) return;
-  deleteTag(tagsList.length-1);
+  if(currentVal || !tagsList.length) return;
+  tagsList[tagsList.length-1].htmlTag.remove();
+  tagsList.pop();
 }
 
-function createTag(tagText) {
-  const tag = document.createElement('div');
-  tag.dataset.index = tagsList.length;
-  tag.classList.add('tag');
-  tag.innerHTML = `
-  <p>${tagText}</p>
-  <span class="material-symbols-outlined tags__icon" >close</span>
-  `;
-  tag.querySelector('.tags__icon').addEventListener('click', (e) => {
-    const index = e.target.parentElement.dataset.index;
-    deleteTag(index);
+function isReapeated(tagText) {
+  return tagsList.includes(tagText);
+}
+
+function renderTag(tagText) {
+  const tag = createTag(tagText);
+  tagsList.push({
+    tagText,
+    htmlTag: tag
   });
+  const tagsWrapper = document.querySelector('.tags__wrapper');
+  tagsWrapper.appendChild(tag);
+}
+
+function createTag(tagText){
+  const tag = document.createElement('div');
+  tag.classList.add('tag');
+  const closeIcon = document.createElement('span');
+  closeIcon.classList.add('material-symbols-outlined', 'tag__close-icon');
+  closeIcon.textContent = 'close'
+  const nodeText = document.createTextNode(tagText);
+  tag.append(nodeText, closeIcon);
+
+  // listern to delete the tag
+  closeIcon.addEventListener('click', () => deleteTag(tagText));
   return tag;
 }
 
-function renderTag(tagText){
-  const tag = createTag(tagText);
-  tagsList.push(tag);
-  const tagsWrapper = tagsComponent.querySelector('.tags__wrapper');
-  tagsWrapper.append(tag);
+function deleteTag(tagValue) {
+  const index = tagsList.findIndex(({tagText}) => tagText === tagValue);
+  tagsList[index].htmlTag.remove();
+  tagsList.splice(index, 1);
 }
-
-function deleteTag(indexTag){
-  if(indexTag < 0) return;
-  tagsList[indexTag].remove();
-}
-
