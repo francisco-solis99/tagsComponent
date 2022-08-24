317a52355821dc0c06b841745b7f0c7387b9fc42
francisco-solis99 <franciscosoliscss@gmail.com>
2022-08-24 12:55:31 -0500

    [Add] the code to delete and add a tag with enter or backspace

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 
 const tagsComponent = document.querySelector('.tags__component');
+const tagsList = [];
 renderComponent();
 function renderComponent(){
   tagsComponent.innerHTML = `
-    <h1 class="tags__title">Add your tags</h1>
+    <h1 class="tags__title">Add your tags (at least 3 and maximum 5)</h1>
     <div class="tags__zone">
       <div class="tags__wrapper"></div>
       <input type="text" class="tags__input">
@@ -13,26 +14,46 @@ function renderComponent(){
 }
 
 function handlerKeyDown(e) {
-  if(e.key.toLowerCase() !== 'enter') return;
-
-  // If enter
+  const key = e.key.toLowerCase();
+  if(key !== 'enter' && key !== 'backspace') return;
   const currentVal = e.target.value;
-  renderTag(currentVal);
-  e.target.value = '';
+
+  // If enter key
+  if(key === 'enter') {
+    if(!currentVal) return;
+    renderTag(currentVal);
+    e.target.value = '';
+    return;
+  }
+  // if backspace key
+  if(currentVal) return;
+  deleteTag(tagsList.length-1);
 }
 
 function createTag(tagText) {
   const tag = document.createElement('div');
+  tag.dataset.index = tagsList.length;
+  tag.classList.add('tag');
   tag.innerHTML = `
-    <p>${tagText}</p>
-    <span class="material-symbols-outlined tags__icon">close</span>
+  <p>${tagText}</p>
+  <span class="material-symbols-outlined tags__icon" >close</span>
   `;
+  tag.querySelector('.tags__icon').addEventListener('click', (e) => {
+    const index = e.target.parentElement.dataset.index;
+    deleteTag(index);
+  });
   return tag;
 }
 
 function renderTag(tagText){
   const tag = createTag(tagText);
+  tagsList.push(tag);
   const tagsWrapper = tagsComponent.querySelector('.tags__wrapper');
   tagsWrapper.append(tag);
+}
+
+function deleteTag(indexTag){
+  if(indexTag < 0) return;
+  tagsList[indexTag].remove();
 }
 
